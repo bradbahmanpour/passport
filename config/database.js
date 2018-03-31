3202edd5fa73e08c64e91accda136b3c5489507d
Brad Bahmanpour <bradbahmanpour@Brads-MacBook-Pro.local>
2018-03-31 00:24:47 -0700

    Completed Passport Authentication App

---
+++ config/database.js
@@ -0,0 +1,13 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://localhost/dive_db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
+
+/*
+// grab the packages we need
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/dive_db');
+*/
